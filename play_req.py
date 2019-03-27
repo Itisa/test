4419236e35718e1b62ea7dd43194ba0d291a3288
haoran song <haoransong@haorandeMacBook-Pro.local>
2019-03-27 22:14:13 +0800

    repair

--- play_req.py
+++ play_req.py
@@ -1,12 +1,7 @@
 #coding:utf-8
 import requests,time
-url = '###'
-url1 = '###'
-name = '###'
-passwd = '###'
 
 r = requests.get(url)
-time.sleep(2)
 
 raw = r.text
 csrf = 'csrfmiddlewaretoken'
@@ -19,16 +14,16 @@ nextstr = nextstr[:nn]
 
 print(nextstr,'\n')
 header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36','Referer':url}
-post_data ={'csrfmiddlewaretoken' : nextstr,'form_username':name,'form_password':passwd}
+post_data ={'csrfmiddlewaretoken' : nextstr,'form_username':name,'form_password':passwd,'next': '/ '}
 
 print(post_data)
-r1 = requests.post(url1,data = post_data)
-#,headers = header)
+r1 = requests.post(url1,data = post_data,headers = header)
 print(r1,'\n')
-print(r1.reason,'\n')
-print('url:',r1.url)
+# print(r1.reason,'\n')
+# print('url:',r1.url)
 print('his',r1.history)
-print(r1.headers,'\n')
-print(r1.request.headers,'\n')
-print(r1.encoding,'\n''\n')
-]
+# print(r1.headers,'\n')
+# print(r1.request.headers,'\n')
+# print(r1.encoding,'\n''\n')
+print(r1.text)
+
