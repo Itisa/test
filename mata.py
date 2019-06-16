53aac53ed7748dd0a131d8763611d7b738dfdefa
haoran song <haoransong@haorandeMacBook-Pro.local>
2019-06-16 20:25:01 +0800

    second

--- mata.py
+++ mata.py
@@ -1,25 +1,15 @@
 import copy,time
+# import pyqt
 
 POSALL = [[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1],[2,1],[1,2]]
 N = 0
 def mata(a,x,y,i=1):
 	global N
 	N += 1
-	# a = a1[:]
-	# a = copy.deepcopy(a1)
-	# if (x,y) in a:
-	# 	return False
-	# else:
-	# 	a.append((x,y))
-	# for i1 in a:
-		# print(i1)
-	# print()
-	# time.sleep(0.1)
 	if a[x][y] == 0:
 		a[x][y] = i
 	else:
 		return False
-		# print(a)
 	if i == 64:
 		return a
 	# for x1,y1 in POSALL:
@@ -61,10 +51,44 @@ def mata(a,x,y,i=1):
 			return t
 	a[x][y] = 0
 	return False
-# a = []
-a = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
-x = 7
-y = 4
+
+def mata_fdd():
+	a = [[0]*8 for i in range(8)]
+	b = [[0]*8 for i in range(8)]
+	x = 0
+	y = 0
+	POSALL = [[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1],[2,1],[1,2]]
+	i = 0
+	i1 = 0
+	x1 = 0
+	y1 = 0
+	while i <= 63:
+		x += x1
+		y += y1
+		# print(i,x,y,x1,y1,b[x][y])
+		n = b[x][y]
+		
+		for x1,y1 in POSALL[b[x][y]:]:
+			n += 1
+			if x+x1<8 and x+x1>-1 and y+y1<8 and y+y1>-1:
+				if a[x+x1][y+y1] != 0:
+					continue
+				else:
+					i += 1
+					a[x+x1][y+y1] = i
+					b[x+x1][y+y1] = n
+					for i2 in a:
+						print(i2)
+					print()
+					# print(i,b[x+x1][y+y1])
+					break
+		if a[x+x1][y+y1] == 0:
+			i -= 1
+			# a[x][y] = 0
+			b[x][y] = 0
+			x1 = -x1
+			y1 = -y1
+
 ti = time.time()
-print(mata(a,x,y))
+print(mata_fdd())
 print(time.time()-ti,N)
--- shudu.py
+++ shudu.py
@@ -17,7 +17,7 @@ def sheng_shudu():
 			else:
 				a[x][y] = i
 				b[y][x] = i
-				c[int(x/3)+int(y/3)*3][x-int(x/3)*3+(y-int(y/3)*3)*3] = i
+				c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = i
 				if x == 8 and y == 8:
 					return a
 				t = shudu(x,y,a,b,c)
@@ -25,7 +25,7 @@ def sheng_shudu():
 					return t
 		a[x][y] = 0
 		b[y][x] = 0
-		c[int(x/3)+int(y/3)*3][x-int(x/3)*3+(y-int(y/3)*3)*3] = 0
+		c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = 0
 		return False
 
 	a = [[0]*9 for i in range(9)]
@@ -34,9 +34,52 @@ def sheng_shudu():
 	r = shudu(-1,0,a,b,c)	
 	return r
 
+def shudu1():
+	a = [[0]*9 for i in range(9)]
+	b = [[0]*9 for i in range(9)]
+	c = [[0]*9 for i in range(9)]
+	d = [[[]]*9 for i in range(9)]
+	x = 0
+	y = 0
+	n = 0
+	while True:
+		n = d[x][y]
+		r = list({1,2,3,4,5,6,7,8,9}-set(n))
+		random.shuffle(r)
+		for i in r:
+			n.append(i)
+			if i in a[x] or i in b[y] or i in c[int(x/3)+int(y/3)*3]:
+				continue
+			else:
+				a[x][y] = i
+				b[y][x] = i
+				c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = i
+				d[x][y] = n
+				break
+		
+		if x == 8 and y == 8:
+			return a
+
+		if a[x][y] == 0:
+			d[x][y] = []
+			if x == 0:
+				x = 8
+				y -= 1
+			else:
+				x -= 1
+			a[x][y] = 0
+			b[y][x] = 0
+			c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = 0
+		else:
+			if x == 8:
+				x = 0
+				y += 1
+			else:
+				x += 1
 
 def zuo_jie_shudu():
-	a = sheng_shudu()
+	# a = sheng_shudu()
+	a = shudu1()
 	for i in a:
 		print(i)
 	print()
@@ -65,7 +108,7 @@ def jie_shudu(jie):
 			else:
 				a[x][y] = i
 				b[y][x] = i
-				c[int(x/3)+int(y/3)*3][x-int(x/3)*3+(y-int(y/3)*3)*3] = i
+				c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = i
 				if x == 8 and y == 8:
 					return a
 				t = shudu(x,y,a,b,c)
@@ -73,7 +116,7 @@ def jie_shudu(jie):
 					return t
 		a[x][y] = 0
 		b[y][x] = 0
-		c[int(x/3)+int(y/3)*3][x-int(x/3)*3+(y-int(y/3)*3)*3] = 0
+		c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = 0
 		return False
 
 	a = [[0]*9 for i in range(9)]
@@ -83,15 +126,84 @@ def jie_shudu(jie):
 		for i1 in range(8):
 			a[i][i1] = jie[i][i1]
 			b[i1][i] = jie[i][i1]
-			c[int(i/3)+int(i1/3)*3][i-int(i/3)*3+(i1-int(i1/3)*3)*3] = jie[i][i1]
+			c[int(i/3)+int(i1/3)*3][i%3+(i1%3)*3] = jie[i][i1]
 
 
 	r = shudu(-1,0,a,b,c)	
 	return r
 
+def jie_shudu1(timu):
+	global YY
+	a = [[0]*9 for i in range(9)]
+	b = [[0]*9 for i in range(9)]
+	c = [[0]*9 for i in range(9)]
+	d = [[[]]*9 for i in range(9)]
+	for i in range(8):
+		for i1 in range(8):
+			a[i][i1] = timu[i][i1]
+			b[i1][i] = timu[i][i1]
+			c[int(i/3)+int(i1/3)*3][i%3+(i1%3)*3] = timu[i][i1]
+	x = 0
+	y = 0
+	n = 0
+	while True:
+		YY += 1
+		if a[x][y] == 0:
+			pass
+		else:
+			if x == 8:
+				x = 0
+				y += 1
+			else:
+				x += 1
+			continue
+		n = d[x][y]
+		r = list({1,2,3,4,5,6,7,8,9}-set(n))
+		for i in r:
+			n.append(i)
+			if i in a[x] or i in b[y] or i in c[int(x/3)+int(y/3)*3]:
+				continue
+			else:
+				a[x][y] = i
+				b[y][x] = i
+				c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = i
+				d[x][y] = n
+				break
+		
+		if x == 8 and y == 8:
+			print('D:')
+			for i in d:
+				print(i)
+			print()
+			return a
+
+		if a[x][y] == 0:
+			d[x][y] = []
+			if x == 0:
+				x = 8
+				y -= 1
+			else:
+				x -= 1
+			while timu[x][y] != 0:
+				if x == 0:
+					x = 8
+					y -= 1
+				else:
+					x -= 1
+			a[x][y] = 0
+			b[y][x] = 0
+			c[int(x/3)+int(y/3)*3][x%3+(y%3)*3] = 0
+		else:
+			if x == 8:
+				x = 0
+				y += 1
+			else:
+				x += 1
+
+
 def ma():
 	ti = zuo_jie_shudu()
-	jie = jie_shudu(ti)
+	jie = jie_shudu1(ti)
 	for i in ti:
 		print(i)
 	print()
@@ -101,3 +213,9 @@ def ma():
 
 if __name__ == '__main__':
 	ma()
+
+	# ans = shudu1()
+	# for i in ans:
+		# for i1 in i:
+			# print(i1)
+		# print()
